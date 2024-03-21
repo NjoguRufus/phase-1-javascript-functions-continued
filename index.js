3b7972899f9f459ffbc56f16389e3732a09fe6d8
NjoguRufus <rufus.njogu@student.moringaschool.com>
2024-03-21 09:58:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+/*SaturdayFun function declaration*/
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+/*MondayWork functionExpression*/
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+/*wrapAdjective Function*/
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
